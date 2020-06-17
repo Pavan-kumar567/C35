72508f84cc45df413aa06aaabf2d46ff05b144b6
Pavan-kumar567 <64518638+Pavan-kumar567@users.noreply.github.com>
2020-06-17 16:48:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var database,playerCount;
+var gameState = 0;
+var form,player,game;
+
+
+function setup(){
+    createCanvas(500,500);
+
+database = firebase.database();
+console.log(database);
+game =new Game();
+game.getState();
+game.start();
+
+}
+
+function draw(){
+    background("white");
+}
